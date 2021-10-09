d8f88e0552681acd9931adac934e90c0068cc4a0
riteshkatukojwala@gmail.com <>
2021-10-09 20:08:20 +0530

    another err

--- .history/index_20211009200715.js
+++ .history/index_20211009200715.js
@@ -1,6 +1,6 @@
 const express=require("express")
 const app=express();
-const cookieParser=require("cookie-parser");
+const cookieParser=requrie("cookie-parser");
 app.use(cookieParser());
 app.get("/",(req,res)=>{
     return res.json({message:"hello messsage"});
--- .history/index_20211009200745.js
+++ .history/index_20211009200745.js
@@ -1,6 +1,6 @@
 const express=require("express")
 const app=express();
-const cookieParser=require("cookie-parser");
+const cookieParser=requie("cookie-parser");
 app.use(cookieParser());
 app.get("/",(req,res)=>{
     return res.json({message:"hello messsage"});
